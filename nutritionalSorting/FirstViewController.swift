ca37d90ba09fef3a62b7f4352da83b4eb2c066ec
Maris Kali <mkali@comcast.net>
2015-11-06 13:53:22 -0800

    Initial Commit

---
+++ nutritionalSorting/FirstViewController.swift
@@ -0,0 +1,25 @@
+//
+//  FirstViewController.swift
+//  nutritionalSorting
+//
+//  Created by Maris Kapualani Kali on 11/6/15.
+//  Copyright © 2015 Maris Kapualani Kali. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
---
+++ nutritionalSorting/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  nutritionalSorting
+//
+//  Created by Maris Kapualani Kali on 11/6/15.
+//  Copyright © 2015 Maris Kapualani Kali. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
